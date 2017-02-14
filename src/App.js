f24bca84c5351548624d8fb991f565d7359fc3d8
Rodney Coleman <rc6886@gmail.com>
2017-02-14 13:18:42 -0600

    Adding completed work thus far on the auth app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCPPnmPh__L9ZLLotxz8dn6Duz-s3T_bPY',
+            authDomain: 'auth-6b92c.firebaseapp.com',
+            databaseURL: 'https://auth-6b92c.firebaseio.com',
+            storageBucket: 'auth-6b92c.appspot.com',
+            messagingSenderId: '545407457796',
+        };
+
+        firebase.initializeApp(config);
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return <CardSection><Button>Log Out</Button></CardSection>;
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>An App!</Text>
+                <Header headerText="Authentication" />
+                {this.renderContent()}
             </View>
         );
     }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+import firebase from 'firebase';
+
+class LoginForm extends Component {
+    state = {
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+    };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '', loading: true });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
+            });
+    }
+
+    onLoginSuccess() {
+        this.setState({ email: '', password: '', loading: false, error: '' });
+    }
+
+    onLoginFail() {
+        this.setState({ error: 'Authentication failed.', loading: false });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Log in
+            </Button>
+        );
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        onChangeText={email => this.setState({ email })}
+                        value={this.state.email}
+                        label="Email"
+                        placeholder="user@gmail.com"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        placeholder="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        label="Password"
+                        secureTextEntry
+                    />
+                </CardSection>
+
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
+                <CardSection>
+                    {this.renderButton()}
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
+export default LoginForm;
